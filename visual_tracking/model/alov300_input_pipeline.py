678578b4df1b3d57117518b06fd97a511a873180
weitian-ding <wt2ding@uwaterloo.ca>
2018-06-07 22:35:48 -0400

    implemented more visualizations

--- visual_tracking/model/alov300_input_pipeline.py
+++ visual_tracking/model/alov300_input_pipeline.py
@@ -1,5 +1,6 @@
 from os import path
 
+from skimage import io, img_as_float32
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -10,47 +11,62 @@ class PairwiseVideoFrameInputFunc(object):
 
     def __init__(self, dataset_dir,
                  batch_size=64,
+                 fixed_frame_size=76,
                  num_epochs=-1,
                  shuffle_buffer_size=1024):
         self.batch_size = batch_size
         self.dataset_dir = dataset_dir
         self.num_epochs = num_epochs
         self.shuffle_bsize = shuffle_buffer_size
+        self.fixed_frame_size = fixed_frame_size
 
     def _read_features_pyfunc(self, feat_dir, prev_bbox, bbox):
         opt_flow_filepath = path.join(feat_dir.decode(), 'optic_flow.mat')
         contrast_filepath = path.join(feat_dir.decode(), 'contrast.npy')
+        prev_frame_filepath = path.join(feat_dir.decode(), 'prev_frame.png')
+        frame_filepath = path.join(feat_dir.decode(), 'frame.png')
 
         # read optic flow
         opt_flow = sio.loadmat(opt_flow_filepath)
-        h_flow = opt_flow['h_flow']
-        v_flow = opt_flow['v_flow']
+        h_flow = opt_flow['h_flow'].astype(np.float32)
+        v_flow = opt_flow['v_flow'].astype(np.float32)
 
         # read contrast
-        contrast = np.load(contrast_filepath)
+        contrast = np.load(contrast_filepath).astype(np.float32)
 
-        # cast to float32
-        h_flow = h_flow.astype(np.float32)
-        v_flow = v_flow.astype(np.float32)
-        contrast = contrast.astype(np.float32)
+        # read frames
+        prev_frame = img_as_float32(io.imread(prev_frame_filepath, as_gray=True))
+        frame = img_as_float32(io.imread(frame_filepath, as_gray=True))
+
+        # cast bounding box to float32
         prev_bbox = prev_bbox.astype(np.float32)
         bbox = bbox.astype(np.float32)
 
-        return h_flow, v_flow, contrast, prev_bbox, bbox
+        return h_flow, v_flow, contrast, prev_bbox, bbox, prev_frame, frame
 
-    def _parse(self, h_flow, v_flow, contrast, prev_bbox, bbox):
-        h_flow.set_shape([78, 78])
-        v_flow.set_shape([78, 78])
-        contrast.set_shape([78, 78])
+    def _parse(self, h_flow, v_flow, contrast, prev_bbox, bbox, prev_frame, frame):
+        h_flow.set_shape([self.fixed_frame_size, self.fixed_frame_size])
+        v_flow.set_shape([self.fixed_frame_size, self.fixed_frame_size])
+        contrast.set_shape([self.fixed_frame_size, self.fixed_frame_size])
         prev_bbox.set_shape([2, 2])
         bbox.set_shape([2, 2])
+
+        prev_frame.set_shape([self.fixed_frame_size, self.fixed_frame_size])
+        prev_frame = tf.reshape(prev_frame, [self.fixed_frame_size, self.fixed_frame_size, 1])
+        frame.set_shape([self.fixed_frame_size, self.fixed_frame_size])
+        frame = tf.reshape(frame, [self.fixed_frame_size, self.fixed_frame_size, 1])
 
         # convert cart to polar coordinates
         speed = tf.sqrt(tf.square(h_flow) + tf.square(v_flow))
         direction = tf.atan(v_flow / h_flow)
         direction = tf.where(tf.is_nan(direction), tf.zeros_like(direction), direction)
 
-        return {'speed': speed, 'direction': direction, 'contrast': contrast, 'prev_bbox': prev_bbox}, bbox
+        return {'speed': speed,
+                'direction': direction,
+                'contrast': contrast,
+                'prev_bbox': prev_bbox,
+                'prev_frame': prev_frame,
+                'frame': frame}, bbox
 
     def __call__(self):
         pairwise_video_frame_df = pd.read_json(path.join(self.dataset_dir, 'index.json'))
@@ -65,7 +81,7 @@ class PairwiseVideoFrameInputFunc(object):
         pairwise_frame_dataset = pairwise_frame_dataset.map(lambda feat_dir, prev_bbox, bbox: tuple(tf.py_func(
             self._read_features_pyfunc,
             [feat_dir, prev_bbox, bbox],
-            [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])))
+            [tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32, tf.float32])))
         pairwise_frame_dataset = pairwise_frame_dataset.map(self._parse)
 
         # make iterator
--- visual_tracking/model/alov300_main.py
+++ visual_tracking/model/alov300_main.py
@@ -6,6 +6,8 @@ import numpy as np
 import MT
 from tuning import SpeedTuning, DirectionTuning
 
+FIXED_FRAME_SIZE = 76 # TODO make this a parameter
+
 tf_sess = tf.Session()
 K.set_session(tf_sess)
 
@@ -20,45 +22,43 @@ params = MT.sample_tuning_params(n_channel, pref_log_speed_range=(0,4))
 
 
 def nn_model_fn(features, labels, mode):
-    # input layers
-    with tf.variable_scope("mt_inputs"):
-        speed_input_layer = tf.reshape(features['speed'], [-1, 78, 78, 1], name='speed_input_tensor')
-        tf.summary.image('speed_input', speed_input_layer)
-        tf.summary.histogram('speed_input', speed_input_layer)
-        direction_input_layer = tf.reshape(features['direction'], [-1, 78, 78, 1], name='direction_input_tensor')
-        contrast_input_layer = tf.reshape(features['contrast'], [-1, 78, 78, 1], name='contrast_input_tensor')
-        tf.summary.image('contrast_input', contrast_input_layer)
+    _draw_input_frames(features, labels)
+
+    contrast_input_layer, direction_input_layer, speed_input_layer = _mt_inputs(features)
 
     # bounding box of the previous frame
-    prev_bbox_input_layer = tf.reshape(features['prev_bbox'], [-1, 4])
+    with tf.variable_scope('prev_bounding_box'):
+        prev_bbox_input_layer = tf.reshape(features['prev_bbox'], [-1, 4])
+        prev_bbox_input_layer = prev_bbox_input_layer / (FIXED_FRAME_SIZE - 1)
 
     # Omid's MT model
-    speed_tun_layer = SpeedTuning(n_channel,
-                                  params,
-                                  100./50 * 25, name='speed_tunning')([speed_input_layer, contrast_input_layer])
-    direction_tun_layer = DirectionTuning(n_channel,
-                                          params,
-                                          name='direction_tunning')(direction_input_layer)
-
-    with tf.variable_scope("mt_activity"):
-        mt_act = tf.multiply(speed_tun_layer, direction_tun_layer, name='MT_act_tensor')
-        mt_act = tf.where(tf.is_nan(mt_act), tf.zeros_like(mt_act), mt_act)
-        tf.summary.histogram('activations', mt_act)
+    mt_act = _mt_model(contrast_input_layer, direction_input_layer, speed_input_layer)
 
     # visual tracking network
-    mt_act_flat = tf.reshape(mt_act, [-1, 78 * 78 * n_channel], name='flattened_MT_act_tensor')
-    concat = tf.concat([mt_act_flat, prev_bbox_input_layer], axis=1)
+    with tf.variable_scope('tracker_nn'):
+        mt_act_flat = tf.reshape(mt_act, [-1, FIXED_FRAME_SIZE * FIXED_FRAME_SIZE * n_channel], name='flattened_mt_act_tensor')
+        concat = tf.concat([mt_act_flat, prev_bbox_input_layer], axis=1)
 
-    dense = tf.layers.dense(inputs=concat, units=4, name='dense_layer')
+        dense = tf.layers.dense(inputs=concat, units=4, name='dense_layer')
+        tf.summary.histogram('dense_layer_before_nonlinear', dense)
+        dense = tf.nn.sigmoid(dense)
+        tf.summary.histogram('dense_layer_nonlinear', dense)
 
-    # reshape it two 2 coordinates
-    predictions = tf.reshape(dense, [-1, 2, 2], name='bbox_tensor')
+        _draw_output_bbox(features['frame'], dense)
+
+        # reshape bounding box to 2x2
+        output = tf.reshape(dense, [-1, 2, 2], name='bbox_scaled')
 
     # PREDICT mode
     if mode == tf.estimator.ModeKeys.PREDICT:
+        # rescale it to coordinates
+        predictions = output * (FIXED_FRAME_SIZE - 1)
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-    loss = tf.losses.mean_squared_error(labels=labels, predictions=predictions)
+    with tf.variable_scope('ground_truth_bbox'):
+        labels_rescaled = labels / (FIXED_FRAME_SIZE - 1)
+
+    loss = tf.losses.absolute_difference(labels=labels_rescaled, predictions=output)
 
     # TRAIN mode
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -68,3 +68,78 @@ def nn_model_fn(features, labels, mode):
             global_step=tf.train.get_or_create_global_step()
         )
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+
+def _mt_model(contrast_input_layer, direction_input_layer, speed_input_layer):
+    with tf.variable_scope('mt_model'):
+
+        speed_tun_layer = SpeedTuning(n_channel,
+                                      params,
+                                      100. / 50 * 25 * 5, name='speed_tunning')([speed_input_layer, contrast_input_layer])
+        direction_tun_layer = DirectionTuning(n_channel,
+                                              params,
+                                              name='direction_tunning')(direction_input_layer)
+
+        # visualize speeding tuning and direction tuning
+        speed_tun_layer_images = tf.reshape(tf.reduce_mean(speed_tun_layer, axis=3),
+                                            [-1, FIXED_FRAME_SIZE, FIXED_FRAME_SIZE, 1])
+        tf.summary.image('speed_tun', speed_tun_layer_images)
+
+        direction_tun_images = tf.reshape(tf.reduce_mean(direction_tun_layer, axis=3),
+                                          [-1, FIXED_FRAME_SIZE, FIXED_FRAME_SIZE, 1])
+        tf.summary.image('direction_tun', direction_tun_images)
+
+
+        with tf.variable_scope("mt_activity"):
+            mt_act = tf.multiply(speed_tun_layer, direction_tun_layer, name='mt_act_tensor')
+            mt_act = tf.where(tf.is_nan(mt_act), tf.zeros_like(mt_act), mt_act)
+
+            tf.summary.histogram('activations', mt_act)
+
+            mt_act_mean = tf.reduce_mean(mt_act, axis=3)
+            mt_act_mean = tf.reshape(mt_act_mean, [-1, FIXED_FRAME_SIZE, FIXED_FRAME_SIZE, 1])
+            tf.summary.image('mt_activities_mean', mt_act_mean)
+
+    return mt_act
+
+
+def _mt_inputs(features):
+    # input layers
+    with tf.variable_scope("mt_inputs"):
+        # speed input
+        speed_input_layer = tf.identity(features['speed'], name='speed_input_tensor')
+        speed_input_images = tf.reshape(speed_input_layer, [-1, FIXED_FRAME_SIZE, FIXED_FRAME_SIZE, 1])
+        tf.summary.image('speed_input', speed_input_images)
+        tf.summary.histogram('speed_input', speed_input_layer)
+
+        # direction input
+        direction_input_layer = tf.identity(features['direction'], name='direction_input_tensor')
+
+        # contrast input
+        contrast_input_layer = tf.identity(features['contrast'], name='contrast_input_tensor')
+        contrast_input_images = tf.reshape(contrast_input_layer, [-1, FIXED_FRAME_SIZE, FIXED_FRAME_SIZE, 1])
+        tf.summary.image('contrast_input', contrast_input_images)
+        tf.summary.histogram('contrast_input', contrast_input_layer)
+
+    return contrast_input_layer, direction_input_layer, speed_input_layer
+
+
+def _draw_input_frames(inputs, labels):
+    with tf.variable_scope('input_frames'):
+        prev_frame = inputs['prev_frame']
+        prev_bbox = tf.reshape(inputs['prev_bbox'], [-1, 1, 4]) / (FIXED_FRAME_SIZE - 1)
+        frame = inputs['frame']
+        ground_truth_bbox = tf.reshape(labels, shape=[-1, 1, 4]) / (FIXED_FRAME_SIZE - 1)
+
+        prev_frame_bbox = tf.image.draw_bounding_boxes(prev_frame, prev_bbox)
+        frame_bbox = tf.image.draw_bounding_boxes(frame, ground_truth_bbox)
+
+        tf.summary.image("frame1", prev_frame_bbox)
+        tf.summary.image("frame2", frame_bbox)
+
+
+def _draw_output_bbox(frame, bbox):
+    with tf.variable_scope('predicted_bbox'):
+        bbox = tf.reshape(bbox, [-1, 1, 4])
+        frame_bbox = tf.image.draw_bounding_boxes(frame, bbox)
+        tf.summary.image("predicted_bbox", frame_bbox)
